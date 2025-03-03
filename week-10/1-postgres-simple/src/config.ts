eebad7cd46c7c97f23cf93a8ae95f2c0d98ec4c5
Priya <ganapathipriyadharsani@gmail.com>
2025-03-03 22:34:10 +0530

    updated week10-postgres

--- week-10/1-postgres-simple/src/config.ts
+++ week-10/1-postgres-simple/src/config.ts
@@ -1,2 +1,2 @@
 
-export const DB_URL = "postgres://postgres:mysecretpassword@localhost/postgres";
+export const DB_URL = "postgresql://neondb_owner:npg_8AO7KcdpPFQY@ep-frosty-king-a8b0d0hv-pooler.eastus2.azure.neon.tech/neondb?sslmode=require";
--- week-10/1-postgres-simple/src/db/user.ts
+++ week-10/1-postgres-simple/src/db/user.ts
@@ -1,5 +1,4 @@
 import { client } from "..";
-
 /*
  * Should insert into the users table
  * Should return the User object
@@ -10,7 +9,14 @@ import { client } from "..";
  * }
  */
 export async function createUser(username: string, password: string, name: string) {
+    const res= await client.query( 
+        `INSERT INTO users (username, password, name) 
+         VALUES ($1, $2, $3) 
+         RETURNING *;`,
+        [username, password, name]
+        );
     
+    return res.rows[0]; 
 }
 
 /*
@@ -22,5 +28,11 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+    const res= await client.query(
+        `SELECT username, password, name FROM users 
+        WHERE id = $1;
+        `,
+        [userId]
+    );
+    return res.rows[0] || null;
 }
